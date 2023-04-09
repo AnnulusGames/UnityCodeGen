26bd98b19ed3b7a0dcff0bf9ae64d05166a3f047
AnnulusGames <annulusgames@gmail.com>
2023-04-09 19:20:18 +0900

    Add: UnityCodeGenUtility

---
+++ Assets/UnityCodeGen/Editor/Core/UnityCodeGenUtility.cs
@@ -0,0 +1,10 @@
+namespace UnityCodeGen
+{
+    public static class UnityCodeGenUtility
+    {
+        public static void Generate()
+        {
+            ScriptFileGenerator.Generate();
+        }
+    }
+}
